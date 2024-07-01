8a49b97e8b5cbd5a59ca7e9b409387ad0474fa1e
Paula <paulasofiasingh@gmail.com>
2024-07-01 16:48:33 -0300

    Se configura puerto a traves de dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = process.env.PORT || 4000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: port,
+    strictPort: true,
+    host: '0.0.0.0',
+  },
 })
