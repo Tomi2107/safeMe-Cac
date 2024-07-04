669a45578a57b20cd05118f828b169c539f23e3e
Paula <paulasofiasingh@gmail.com>
2024-07-04 12:33:16 -0300

    Se añadieron validaciones al formulario

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -9,6 +9,8 @@ const Registro = () => {
     contraseña2: "",
   });
 
+  const [errores, setErrores] = useState({});
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -17,17 +19,51 @@ const Registro = () => {
     });
   };
 
-  const onSubmit = (e)=> {
+  const onSubmit = (e) => {
     e.preventDefault();
-    console.log(formData)
-  }
+    const errores = validarFormulario(formData);
+    if (Object.keys(errores).length === 0) {
+      console.log("Datos del formulario:", formData);
+      setFormData({
+        nombre: "",
+        apellido: "",
+        email: "",
+        contraseña: "",
+        contraseña2: "",
+      });
+    } else {
+      setErrores(errores);
+    }
+  };
+
+  const validarFormulario = (values) => {
+    let errores = {};
+    if (!values.nombre.trim()) {
+      errores.nombre = "El nombre es requerido";
+    }
+    if (!values.apellido.trim()) {
+      errores.apellido = "El apellido es requerido";
+    }
+    if (!values.email.trim()) {
+      errores.email = "El email es requerido";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+      errores.email = "El email no es válido";
+    }
+    if (!values.contraseña.trim()) {
+      errores.contraseña = "La contraseña es requerida";
+    }
+    if (values.contraseña !== values.contraseña2) {
+      errores.contraseña2 = "Las contraseñas no coinciden";
+    }
+    return errores;
+  };
 
   return (
     <>
       <h1>CREA TU CUENTA</h1>
       <h2>Registra tus datos</h2>
       <form /* onSubmit={handleSubmit} */>
-        <label htmlFor ="nombre">Nombre:</label>
+        <label htmlFor="nombre">Nombre:</label>
         <input
           type="text"
           name="nombre"
@@ -35,8 +71,11 @@ const Registro = () => {
           value={formData.nombre}
           onChange={handleChange}
         />
+        {errores.nombre && (
+          <span style={{ color: "red" }}>{errores.nombre}</span>
+        )}
         <br></br>
-        <label htmlFor ="apellido">Apellido:</label>
+        <label htmlFor="apellido">Apellido:</label>
         <input
           type="text"
           name="apellido"
@@ -44,8 +83,11 @@ const Registro = () => {
           value={formData.apellido}
           onChange={handleChange}
         />
+        {errores.apellido && (
+          <span style={{ color: "red" }}>{errores.apellido}</span>
+        )}
         <br></br>
-        <label htmlFor ="email">E-mail:</label>
+        <label htmlFor="email">E-mail:</label>
         <input
           type="text"
           name="email"
@@ -53,26 +95,33 @@ const Registro = () => {
           value={formData.email}
           onChange={handleChange}
         />
+        {errores.email && <span style={{ color: "red" }}>{errores.email}</span>}
         <br></br>
-        <label htmlFor ="contraseña">Contraseña:</label>
+        <label htmlFor="contraseña">Contraseña:</label>
         <input
-          type="text"
+          type="password"
           name="contraseña"
           id="contraseña"
           value={formData.contraseña}
           onChange={handleChange}
         />
+        {errores.contraseña && (
+          <span style={{ color: "red" }}>{errores.contraseña}</span>
+        )}
         <br></br>
-        <label htmlFor ="contraseña2">Repita contraseña:</label>
+        <label htmlFor="contraseña2">Repita contraseña:</label>
         <input
-          type="text"
+          type="password"
           name="contraseña2"
           id="contraseña2"
           value={formData.contraseña2}
           onChange={handleChange}
         />
+        {errores.contraseña2 && (
+          <span style={{ color: "red" }}>{errores.contraseña2}</span>
+        )}
         <br></br>
-{/*         <input onClick={onSubmit} value="Registrar"/> */}
+        {/*         <input onClick={onSubmit} value="Registrar"/> */}
         <button onClick={onSubmit}>Registrar</button>
       </form>
     </>
