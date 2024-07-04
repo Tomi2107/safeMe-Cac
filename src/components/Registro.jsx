2c5318d8fff41a58dfa299001c58d1dfe6ac2c95
Paula <paulasofiasingh@gmail.com>
2024-07-04 11:52:09 -0300

    Se agrego funcionalidad al boton del formulario para que tome los datos de los input. Se creo un useState que controla el estado del formulario

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,44 +1,82 @@
-const Registro = ()=> {
+import { useState } from "react";
 
-    return (
-        <>
-        <h1>CREA TU CUENTA</h1>
-        <h2>Registra tus datos</h2>
-        <form /* onSubmit={handleSubmit} */>
-          <label for="nombre">Nombre:</label>
-          <input
-            type="text"
-            name="nombre"
-            id="nombre"
-          /><br></br>
-          <label for="apellido">Apellido:</label>
-          <input
-            type="text"
-            name="apellido"
-            id="apellido"
-          /><br></br>
-          <label for="email">E-mail:</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-          /><br></br>
-          <label for="contraseña">Contraseña:</label>
-          <input
-            type="text"
-            name="contraseña"
-            id="contraseña"
-          /><br></br>
-          <label for="contraseña2">Repita contraseña:</label>
-          <input
-            type="text"
-            name="contraseña2"
-            id="contraseña2"
-          /><br></br>
-        <button type="submit">Registrar</button>
+const Registro = () => {
+  const [formData, setFormData] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+    contraseña: "",
+    contraseña2: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = (e)=> {
+    e.preventDefault();
+    console.log(formData)
+  }
+
+  return (
+    <>
+      <h1>CREA TU CUENTA</h1>
+      <h2>Registra tus datos</h2>
+      <form /* onSubmit={handleSubmit} */>
+        <label htmlFor ="nombre">Nombre:</label>
+        <input
+          type="text"
+          name="nombre"
+          id="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+        />
+        <br></br>
+        <label htmlFor ="apellido">Apellido:</label>
+        <input
+          type="text"
+          name="apellido"
+          id="apellido"
+          value={formData.apellido}
+          onChange={handleChange}
+        />
+        <br></br>
+        <label htmlFor ="email">E-mail:</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <br></br>
+        <label htmlFor ="contraseña">Contraseña:</label>
+        <input
+          type="text"
+          name="contraseña"
+          id="contraseña"
+          value={formData.contraseña}
+          onChange={handleChange}
+        />
+        <br></br>
+        <label htmlFor ="contraseña2">Repita contraseña:</label>
+        <input
+          type="text"
+          name="contraseña2"
+          id="contraseña2"
+          value={formData.contraseña2}
+          onChange={handleChange}
+        />
+        <br></br>
+{/*         <input onClick={onSubmit} value="Registrar"/> */}
+        <button onClick={onSubmit}>Registrar</button>
       </form>
-      </>
-    );
-}
+    </>
+  );
+};
 
-export default Registro
+export default Registro;
