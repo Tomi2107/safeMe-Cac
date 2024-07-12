aa44855dcbe65eda70d7a9a859cab9544cb80318
Paula <paulasofiasingh@gmail.com>
2024-07-12 16:49:53 -0300

    Se establecio que los valores de los selectores se actualicen con los contactos guardados y se muestren correctamente en la interfaz de usuario

--- src/components/Alerts.jsx
+++ src/components/Alerts.jsx
@@ -1,21 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { useAuth } from '../context/authContext';
-import { db } from "../firebaseConfig/firebase"
-import { collection, getDocs, doc, setDoc, updateDoc, query, where, getDoc } from 'firebase/firestore';
-import { MainHeader } from './MainHeader'; 
-import { Footer } from './Footer';
-import { Link } from 'react-router-dom';
-import "./Alerts.css"
-
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../context/authContext";
+import { db } from "../firebaseConfig/firebase";
+import {
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+  updateDoc,
+  query,
+  where,
+  getDoc,
+} from "firebase/firestore";
+import { MainHeader } from "./MainHeader";
+import { Footer } from "./Footer";
+import { Link } from "react-router-dom";
+import "./Alerts.css";
 
 export const Alerts = () => {
-
   const [contactos, setContactos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [selectedContactoSeguridad, setSelectedContactoSeguridad] = useState('');
-  const [selectedContactoSalud, setSelectedContactoSalud] = useState('');
-  const [mensajeSeguridad, setMensajeSeguridad] = useState('');
-  const [mensajeSalud, setMensajeSalud] = useState('');
+  const [selectedContactoSeguridad, setSelectedContactoSeguridad] =
+    useState("");
+  const [selectedContactoSalud, setSelectedContactoSalud] = useState("");
+  const [mensajeSeguridad, setMensajeSeguridad] = useState("");
+  const [mensajeSalud, setMensajeSalud] = useState("");
   const [alertasSeguridad, setAlertasSeguridad] = useState({}); // Initialize as an empty object
   const [alertasSalud, setAlertasSalud] = useState({}); // Initialize as an empty object
   const { user } = useAuth();
@@ -25,42 +33,58 @@ export const Alerts = () => {
   useEffect(() => {
     const fetchContactos = async () => {
       try {
-        const contactosQuery = query(collection(db, 'contactos'), where('usuarioId', '==', userId));
+        const contactosQuery = query(
+          collection(db, "contactos"),
+          where("usuarioId", "==", userId)
+        );
         const contactosSnap = await getDocs(contactosQuery);
         const contactosData = contactosSnap.docs.map((doc) => doc.data());
         setContactos(contactosData);
         setIsLoading(false);
       } catch (error) {
-        console.error('Error fetching contacts:', error);
+        console.error("Error fetching contacts:", error);
         // You can display an error message to the user here
       }
     };
 
     const fetchAlertas = async () => {
       try {
-        const mensajesSeguridadRef = doc(db, 'mensajesAlertaSeguridad', userId);
-        const mensajesSaludRef = doc(db, 'mensajesAlertaSalud', userId);
+        const mensajesSeguridadRef = doc(db, "mensajesAlertaSeguridad", userId);
+        const mensajesSaludRef = doc(db, "mensajesAlertaSalud", userId);
 
         const mensajesSeguridadSnap = await getDoc(mensajesSeguridadRef);
         const mensajesSaludSnap = await getDoc(mensajesSaludRef);
 
-        const mensajeSeguridadData = mensajesSeguridadSnap.exists() ? mensajesSeguridadSnap.data() : {};
-        const mensajeSaludData = mensajesSaludSnap.exists() ? mensajesSaludSnap.data() : {};
+        const mensajeSeguridadData = mensajesSeguridadSnap.exists()
+          ? mensajesSeguridadSnap.data()
+          : {};
+        const mensajeSaludData = mensajesSaludSnap.exists()
+          ? mensajesSaludSnap.data()
+          : {};
 
         setAlertasSeguridad(mensajeSeguridadData);
         setAlertasSalud(mensajeSaludData);
 
         // Para que muestre los mensajes que ya están creados
-        if (mensajeSeguridadData.mensaje) setMensajeSeguridad(mensajeSeguridadData.mensaje);
+        if (mensajeSeguridadData.mensaje)
+          setMensajeSeguridad(mensajeSeguridadData.mensaje);
         if (mensajeSaludData.mensaje) setMensajeSalud(mensajeSaludData.mensaje);
+
+        // Establecer los contactos seleccionados
+        if (mensajeSeguridadData.contactoId)
+          setSelectedContactoSeguridad(mensajeSeguridadData.contactoId);
+        if (mensajeSaludData.contactoId)
+          setSelectedContactoSalud(mensajeSaludData.contactoId);
       } catch (error) {
-        console.error('Error fetching alerts:', error);
+        console.error("Error fetching alerts:", error);
         // You can display an error message to the user here
       }
     };
 
-    fetchContactos();
-    fetchAlertas();
+    if (userId) { // Me aseguro de que fetchContactos y fetchAlertas solo se llamen cuando userId esté disponible
+      fetchContactos();
+      fetchAlertas();
+    }
   }, [user, db]);
 
   const handleContactoChangeSeguridad = (event) => {
@@ -81,12 +105,12 @@ export const Alerts = () => {
 
   const handleAddAlertas = async () => {
     if (!user) {
-      alert('Debes iniciar sesión para agregar alertas.');
+      alert("Debes iniciar sesión para agregar alertas.");
       return;
     }
 
-    const mensajesSeguridadRef = doc(db, 'mensajesAlertaSeguridad', userId);
-    const mensajesSaludRef = doc(db, 'mensajesAlertaSalud', userId);
+    const mensajesSeguridadRef = doc(db, "mensajesAlertaSeguridad", userId);
+    const mensajesSaludRef = doc(db, "mensajesAlertaSalud", userId);
 
     try {
       // Update existing messages (if any)
@@ -107,13 +131,12 @@ export const Alerts = () => {
         });
       }
 
-      alert('Alertas guardadas correctamente.');
+      alert("Alertas guardadas correctamente.");
     } catch (error) {
-      console.error('Error saving alerts:', error);
-      alert('Error al guardar las alertas.');
+      console.error("Error saving alerts:", error);
+      alert("Error al guardar las alertas.");
     }
   };
-  
 
   return (
     <>
@@ -125,7 +148,11 @@ export const Alerts = () => {
             {isLoading ? (
               <p>Cargando contactos...</p>
             ) : (
-              <select value={selectedContactoSeguridad} onChange={handleContactoChangeSeguridad} required>
+              <select
+                value={selectedContactoSeguridad}
+                onChange={handleContactoChangeSeguridad}
+                required
+              >
                 <option value="">Seleccionar Contacto</option>
                 {contactos.map((contacto) => (
                   <option key={contacto.id} value={contacto.id}>
@@ -136,14 +163,23 @@ export const Alerts = () => {
             )}
           </div>
           <div>
-            <textarea value={mensajeSeguridad} onChange={handleMensajeSeguridadChange} placeholder="Ingresar mensaje de alerta" required />
+            <textarea
+              value={mensajeSeguridad}
+              onChange={handleMensajeSeguridadChange}
+              placeholder="Ingresar mensaje de alerta"
+              required
+            />
           </div>
           <div>
             <h2>Alertas de Salud</h2>
             {isLoading ? (
               <p>Cargando contactos...</p>
             ) : (
-              <select value={selectedContactoSalud} onChange={handleContactoChangeSalud} required>
+              <select
+                value={selectedContactoSalud}
+                onChange={handleContactoChangeSalud}
+                required
+              >
                 <option value="">Seleccionar Contacto</option>
                 {contactos.map((contacto) => (
                   <option key={contacto.id} value={contacto.id}>
@@ -154,7 +190,12 @@ export const Alerts = () => {
             )}
           </div>
           <div>
-            <textarea value={mensajeSalud} onChange={handleMensajeSaludChange} placeholder="Ingresar mensaje de alerta" required />
+            <textarea
+              value={mensajeSalud}
+              onChange={handleMensajeSaludChange}
+              placeholder="Ingresar mensaje de alerta"
+              required
+            />
             <button onClick={handleAddAlertas}>Agregar Alerta</button>
           </div>
           <Link to="/">
