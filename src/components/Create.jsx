a7e9073a219ba2dd6c7674ccd0f5d1b52da4d2f2
XianPaz <cmezzano@gmail.com>
2024-07-11 22:16:31 -0300

    fix: create css adjustments

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -60,72 +60,64 @@ export const Create = () => {
     <>
       <MainHeader />
 
-      <div className="container-create">
-        <h1 className="text-center">CONTACTO</h1>
-        <h2 className="text-center">Registra a tu contacto</h2>
-        <form onSubmit={createContacto} className="form-container">
-          <div>
-            <label htmlFor="nombre" className="form-label">
-              Nombre:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="nombre"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-              required
-            />
-          </div>
+      <div className="create__container">
+        <h1 className="create__title">CONTACTO</h1>
+        <h2 className="create__subtitle">Registra a tu contacto</h2>
+        <form onSubmit={createContacto} className="create__form">
+          <label htmlFor="nombre" className="form-label">
+            Nombre:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            required
+          />
 
-          <div className="mb-3">
-            <label htmlFor="apellido" className="form-label">
-              Apellido:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="apellido"
-              value={apellido}
-              onChange={(e) => setApellido(e.target.value)}
-              required
-            />
-          </div>
+          <label htmlFor="apellido" className="form-label">
+            Apellido:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="apellido"
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+            required
+          />
 
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email:
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-          <div className="mb-3">
-            <label htmlFor="telefono" className="form-label">
-              Teléfono:
-            </label>
-            <input
-              type="tel"
-              className="form-control"
-              id="telefono"
-              value={telefono}
-              onChange={(e) => setTelefono(e.target.value)}
-              required
-            />
-          </div>
+          <label htmlFor="telefono" className="form-label">
+            Teléfono:
+          </label>
+          <input
+            type="tel"
+            className="form-control"
+            id="telefono"
+            value={telefono}
+            onChange={(e) => setTelefono(e.target.value)}
+            required
+          />
 
-          <div className="button-group">
-            <button type="submit" className="btn btn-primary">
+          <div className="create__buttons">
+            <button type="submit" className="btn">
               Registrar
             </button>
             <Link to="/contacts">
-              <button type="button" className="btn btn-danger">
+              <button type="button" className="btn">
                 CANCELAR
               </button>
             </Link>
